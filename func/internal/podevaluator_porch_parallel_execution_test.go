777b9e2d93f379f06f71a82ed665443d0d87cab5
Döme Matusovits <dome.matusovits@nokia.com>
2026-04-15 15:45:31 +0200

    Further development + fixing copyright dates

--- func/internal/podevaluator_porch_parallel_execution_test.go
+++ func/internal/podevaluator_porch_parallel_execution_test.go
@@ -1,3 +1,17 @@
+// Copyright 2026 The kpt and Nephio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package internal
 
 import (
@@ -26,6 +40,9 @@ type fakeFnEvalServer struct {
 
 func (s *fakeFnEvalServer) EvaluateFunction(_ context.Context, _ *pb.EvaluateFunctionRequest) (*pb.EvaluateFunctionResponse, error) {
 	time.Sleep(s.delay)
+	if s.evalErr != nil {
+		return nil, s.evalErr
+	}
 	return &pb.EvaluateFunctionResponse{ResourceList: []byte("ok"), Log: []byte("log")}, nil
 }
 
